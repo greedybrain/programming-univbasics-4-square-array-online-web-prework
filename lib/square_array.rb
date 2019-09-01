591ba9a9f31c837967688e7e44f1791aa3309afa
sensible-gateway-6049 <greedybrain18@gmail.com>
2019-09-01 21:58:26 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,7 +1,10 @@
 def square_array(array)
   counter = 0
+  new_arr = []
   while counter < array.length do
-    p array[counter] ** 2
-    counter + 1
+    new_arr << array[counter] ** 2
+    counter += 1
   end
+  
+  print new_arr
 end
